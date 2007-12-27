494c045c0530f5b8e80a2d2d67c320a74ed88157
martinp23 <martinp23@613dd46a-1b32-0410-9e20-f3d8e65474cc>
2007-12-27 18:32:10 +0000

    Copyright notices

--- NPW/NPWatcher/Main.cs
+++ NPW/NPWatcher/Main.cs
@@ -1,3 +1,27 @@
+/*
+From: http://svn.martinp23.com/npw/
+
+Copyright 2007 Martin Peeks
+Copyright 2007 Reedy_Boy
+Copyright 2007 Martijn Hoekstra
+
+This file is part of NPWatcher.
+
+NPWatcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+NPWatcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with NPWatcher.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
--- NPW/NPWatcher/Properties/AssemblyInfo.cs
+++ NPW/NPWatcher/Properties/AssemblyInfo.cs
@@ -1,4 +1,26 @@
-﻿using System.Reflection;
+﻿/*
+From: http://svn.martinp23.com/npw/
+
+Copyright 2007 Martin Peeks
+
+This file is part of NPWatcher.
+
+NPWatcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+NPWatcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with NPWatcher.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
